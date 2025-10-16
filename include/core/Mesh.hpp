1d77bd6da97cbf5aa0e5286af78c243eb8261be2
Daniele Piano <dpiano01@gmail.com>
2025-10-16 14:47:52 +0200

    Adding size method to Grid struct. Definition of the Derivatives handler class interface.

--- include/core/Mesh.hpp
+++ include/core/Mesh.hpp
@@ -12,7 +12,18 @@ struct Grid {
     double dx, dy, dz; // Grid spacing in each direction
 
     Grid(size_t Nx, size_t Ny, size_t Nz, double dx, double dy, double dz)
-            : Nx(Nx), Ny(Ny), Nz(Nz), dx(dx), dy(dy), dz(dz) {}
+            : Nx(Nx), Ny(Ny), Nz(Nz), dx(dx), dy(dy), dz(dz) {};
+
+    /**
+     * @brief Getter for the total number of grid points.
+     * @return the total number of grid points
+     */
+    size_t size() { return Nx * Ny * Nz; }
+
+    /**
+     * @overload
+     */
+    size_t size() const { return Nx * Ny * Nz; }
 };
 
 // todo - TBD: Mesh class ?? methods for grid generation ??
