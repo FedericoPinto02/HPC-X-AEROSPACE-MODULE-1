ce3f89ff64df6e626429d34e4213ceef5e515a75
Daniele Piano <dpiano01@gmail.com>
2025-11-22 20:54:34 +0100

    (wip) SimulationData: not use shared pointer for Grid anymore, use Functions::Func for forcing term and boundary condition functions

--- include/simulation/SimulationContext.hpp
+++ include/simulation/SimulationContext.hpp
@@ -30,11 +30,8 @@ struct ParallelizationSettings {
 
 struct SimulationData {
     // Grid
-    std::shared_ptr<const Grid> gridPtr;
-    double dx, dy, dz;
-    double Lx, Ly, Lz;
-    size_t Nx, Ny, Nz;
-    
+    Grid grid;
+
     // Time integration settings
     double dt;
     double currTime;
@@ -53,9 +50,9 @@ struct SimulationData {
     // Boundary conditions
     VectorField uBoundOld;
     VectorField uBoundNew;
-    std::function<double (double t, double x, double y, double z)> bcu;
-    std::function<double (double t, double x, double y, double z)> bcv;
-    std::function<double (double t, double x, double y, double z)> bcw;
+    Functions::Func bcu;
+    Functions::Func bcv;
+    Functions::Func bcw;
 
     // Kinematic viscosity of the fluid
     double nu;
@@ -65,9 +62,9 @@ struct SimulationData {
 
     // Body force acting on the medium
     VectorField f;
-    std::function<double (double t, double x, double y, double z)> fx;
-    std::function<double (double t, double x, double y, double z)> fy;
-    std::function<double (double t, double x, double y, double z)> fz;
+    Functions::Func fx;
+    Functions::Func fy;
+    Functions::Func fz;
 };
 
 #endif //NSBSOLVER_SIMULATIONCONTEXT_HPP
