5a3e59ecbaa732a9974745f57487353235e0147f
StealthyGotti <mattia2002a@gmail.com>
2025-11-23 17:56:29 +0100

    profiling update

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 BUILD_DIR="build"
-CMAKE_FLAGS="-DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF"
+CMAKE_FLAGS="-DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF -DPROFILING=ON"
 
 mkdir -p "$BUILD_DIR"
 pushd "$BUILD_DIR" > /dev/null
